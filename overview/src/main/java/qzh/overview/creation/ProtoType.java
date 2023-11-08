00aef6c9589f586932f64c4c2a6fe4e53f1b935f
zhihao.qu <zhihao.qu@ly.com>
2023-11-08 22:38:54 +0800

    feat: 原型模式

--- overview/src/main/java/qzh/overview/creation/ProtoType.java
+++ overview/src/main/java/qzh/overview/creation/ProtoType.java
@@ -1,10 +1,37 @@
 package qzh.overview.creation;
 
+import lombok.Data;
+
 /**
  * 原型模式：创建重复的对象，又能保持性能
+ * 场景：
+ * 1. 资源优化
+ * 2. 性能和安全要求
+ * 3. 一个对象有多个修改者
+ * 4. 深拷贝、浅拷贝
  */
 public class ProtoType {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Proto proto = new Proto();
+        proto.setName("Proto");
+        proto.setAge(11);
+        Proto cloneObj = proto.clone();
+        System.out.println(cloneObj.getAge());
+        System.out.println(cloneObj.getName());
+    }
+
+    @Data
+    public static class Proto implements Cloneable {
+        private String name;
+        private int age;
+
+        @Override
+        protected Proto clone() throws CloneNotSupportedException {
+            Proto proto = new Proto();
+            proto.setAge(this.age);
+            proto.setName(this.name);
+            return proto;
+        }
     }
 }
